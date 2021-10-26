90fc87a96305baa0d14f2949684418d5df8832ad
BenjaElDelGitHub <83060244+BenjaElDelGitHub@users.noreply.github.com>
2021-10-26 10:06:11 +0200

    Add files via upload

--- Hija.py
+++ Hija.py
@@ -1,14 +1,14 @@
 from Polimorfismo.Padre import Animal
 
 
-class perro(Animal):
+class Perro(Animal):
     def andar(self):
         print("Estoy andando a 4 patas")
 
-class pato(Animal):
+class Pato(Animal):
     def andar(self):
         print("Estoy andando a 2 patas")
 
 
-for Animal in perro(), pato():
+for Animal in Perro(), Pato():
     Animal.andar()
